8fa7b7934da850d0266fca05a20e6524e4c114e1
Tom Amaral <thomasamaral2016@gmail.com>
2021-04-21 19:51:13 -0400

    A very simple module to utilize the results from scrape.py and insert them all into a mongo collection all at once

--- db-builder/db.py
+++ db-builder/db.py
@@ -2,10 +2,19 @@
     db.py
     Tom Amaral <thomasamaral2016@gmail.com>
 
-    A python wrapper for Mongo CRUD operations
+    A (very simple) python module to build a mongodb collection based off the
+    web scraping results from scrape.py
 """
 
 import pymongo
+from scrape import get_kanto
+
+MONGO_CLIENT = pymongo.MongoClient("mongodb://localhost:27017/")
+POKE_DB = MONGO_CLIENT["pokedex"]
+POKE_COLLECTION = POKE_DB["pokemon"]
 
 
-
+if __name__ == "__main__": 
+    kanto_objects = get_kanto()
+    result = POKE_COLLECTION.insert_many(kanto_objects)
+    print(f'Inserted {len(result.inserted_ids)} entries into the Pokedex collection')
